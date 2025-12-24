327aebf0af5c3b4208ee2ebc79cc70394bdd5c79
Xarathos <xarathos.business@gmail.com>
2025-12-24 20:26:49 +0200

    main holds only info

--- src/main/java/com/sse3/gamesense/GameSenseMod/api/events/IEventHandler.java
+++
@@ -1,9 +0,0 @@
-/*
- * Decompiled with CFR 0.152.
- */
-package com.sse3.gamesense.GameSenseMod.api.events;
-
-public interface IEventHandler<T> {
-    public void handle(T var1);
-}
-
--- src/main/java/com/sse3/gamesense/GameSenseMod/api/events/IPlayerTickEvent.java
+++
@@ -1,7 +0,0 @@
-package com.sse3.gamesense.GameSenseMod.api.events;
-
-import net.minecraft.entity.LivingEntity;
-
-public interface IPlayerTickEvent {
-    LivingEntity getPlayer();
-}
